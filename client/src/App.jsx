30028b2028b76c272efb20c6dbf13947526e8690
Carrotymusic <shumskuy.15.71@gmail.com>
2025-12-05 18:36:52 +0100

    Fix router basename for GitHub Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/AnonStyle">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
